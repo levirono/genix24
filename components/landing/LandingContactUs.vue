5cfad898905002d964ab5c3d31d1e93b92b26584
levi <ronolawi3@gmail.com>
2024-08-01 13:08:51 +0300

    new change

--- components/landing/LandingContactUs.vue
+++ components/landing/LandingContactUs.vue
@@ -10,25 +10,7 @@
           <p class="text-gray-600 dark:text-gray-400">{{ method.description }}</p>
         </div>
       </div>
-      <!-- <div class="mt-12">
-        <form @submit.prevent="submitForm" class="max-w-lg mx-auto">
-          <div class="mb-4">
-            <label for="name" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Name</label>
-            <input type="text" id="name" v-model="form.name" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-          </div>
-          <div class="mb-4">
-            <label for="email" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Email</label>
-            <input type="email" id="email" v-model="form.email" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-          </div>
-          <div class="mb-4">
-            <label for="message" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Message</label>
-            <textarea id="message" v-model="form.message" rows="4" required class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"></textarea>
-          </div>
-          <div class="text-center">
-            <button type="submit" class="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Send Message</button>
-          </div>
-        </form>
-      </div> -->
+      <!--    -->
     </section>
   </template>
   
--- components/landing/LandingFeatues.vue
+++ components/landing/LandingFeatues.vue
@@ -1,37 +1,40 @@
 <template>
-    <section class="mt-20">
-      <UContainer>
-        <div v-for="(feature, index) in features" :key="index" class="flex flex-col md:flex-row items-center mb-16 glassmorphism p-8 rounded-lg" :class="{ 'md:flex-row-reverse': index % 2 !== 0 }">
-          <div class="md:w-1/2 md:pr-8 mb-8 md:mb-0" :class="{ 'md:pl-8': index % 2 !== 0 }">
-            <h3 class="text-3xl font-bold mb-4 bg-clip-text text-transparent" :class="feature.gradientClass">{{ feature.title }}</h3>
-            <p class="text-gray-600 dark:text-gray-400">
-              {{ feature.description }}
-            </p>
-          </div>
-          <div class="md:w-1/2">
-            <div class="w-full h-64 bg-gray-300 rounded-lg shadow-lg"></div>
-          </div>
+  <section class="mt-20">
+    <UContainer>
+      <div v-for="(feature, index) in features" :key="index" class="flex flex-col md:flex-row items-center mb-16 glassmorphism p-8 rounded-lg" :class="{ 'md:flex-row-reverse': index % 2 !== 0 }">
+        <div class="md:w-1/2 md:pr-8 mb-8 md:mb-0" :class="{ 'md:pl-8': index % 2 !== 0 }">
+          <h3 class="text-3xl font-bold mb-4 bg-clip-text text-transparent" :class="feature.gradientClass">{{ feature.title }}</h3>
+          <p class="text-gray-600 dark:text-gray-400">
+            {{ feature.description }}
+          </p>
         </div>
-      </UContainer>
-    </section>
-  </template>
-  
-  <script setup>
-  const features = [
-    {
-      title: "Innovative Solutions",
-      description: "At Genixl, we're dedicated to pushing the boundaries of technology. Our team of experts works tirelessly to create solutions that transform industries and improve lives.",
-      gradientClass: "bg-gradient-to-r from-blue-500 to-emerald-500"
-    },
-    {
-      title: "Customer-Centric Approach",
-      description: "We believe in putting our customers first. Our products and services are designed with your needs in mind, ensuring that we deliver value and exceed expectations at every turn.",
-      gradientClass: "bg-gradient-to-r from-amber-500 to-red-500"
-    },
-    {
-      title: "Sustainable Future",
-      description: "At Genixl, we're committed to creating a sustainable future. Our eco-friendly practices and innovative green technologies are designed to minimize our environmental impact while maximizing efficiency.",
-      gradientClass: "bg-gradient-to-r from-blue-500 to-emerald-500"
-    }
-  ];
-  </script>
+        <div class="md:w-1/2">
+          <img :src="feature.image" :alt="feature.title" class="w-full h-full object-cover rounded-lg shadow-lg" />
+        </div>
+      </div>
+    </UContainer>
+  </section>
+</template>
+
+<script setup>
+const features = [
+  {
+    title: "Innovative Solutions",
+    description: "At Genixl, we're dedicated to pushing the boundaries of technology. Our team of experts works tirelessly to create solutions that transform industries and improve lives.",
+    image: "innovation.jpeg",
+    gradientClass: "bg-gradient-to-r from-blue-500 to-emerald-500"
+  },
+  {
+    title: "Customer-Centric Approach",
+    description: "We believe in putting our customers first. Our products and services are designed with your needs in mind, ensuring that we deliver value and exceed expectations at every turn.",
+    image: "centric.jpeg",
+    gradientClass: "bg-gradient-to-r from-amber-500 to-red-500"
+  },
+  {
+    title: "Sustainable Future",
+    description: "At Genixl, we're committed to creating a sustainable future. Our eco-friendly practices and innovative green technologies are designed to minimize our environmental impact while maximizing efficiency.",
+    image: "sustain.jpeg",
+    gradientClass: "bg-gradient-to-r from-blue-500 to-emerald-500"
+  }
+];
+</script>
