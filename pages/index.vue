6dd8f2990011a94ad4fdbf4586d0044bfdaa1eca
levirono <ronolawi3@gmail.com>
2025-06-09 11:37:41 +0300

    new adjustment

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,18 @@
 <template>
-    <div class="min-h-screen flex flex-col bg-gradient-to-br from-gray-100 to-gray-200 text-gray-800 dark:bg-gradient-to-br dark:from-gray-900 dark:to-gray-800 dark:text-gray-200">
-      <main class="flex-1 px-4 py-12 md:px-8">
-        <LandingPageHero />
-        <LandingFeatues />
-        <LandingProducts />
-        <WhyChooseUs />
-        <LandingAboutUs />
-        <LandingContactUs/>
-      </main>
-    </div>
-  </template>
-  
-  <script setup>
+  <div
+    class="min-h-screen flex flex-col bg-gradient-to-br from-gray-100 to-gray-200 text-gray-800 dark:bg-gradient-to-br dark:from-gray-900 dark:to-gray-800 dark:text-gray-200">
+    <main class="flex-1 px-4 py-12 md:px-8">
+      <LandingPageHero />
+      <LandingFeatues />
+      <LandingProducts />
+      <WhyChooseUs />
+      <LandingAboutUs />
+      <LandingContactUs />
+    </main>
+  </div>
+</template>
+
+<script setup>
 useHead({
   link: [
     { rel: 'canonical', href: 'https://genix24.vercel.app/' }
@@ -44,27 +45,35 @@ useHead({
 //   updateTheme();
 // });
 // </script>
-  
-  <style>
-  .glassmorphism {
-    background: rgba(255, 255, 255, 0.2);
-    backdrop-filter: blur(10px);
-    border-radius: 10px;
-    border: 1px solid rgba(255, 255, 255, 0.3);
+
+<style>
+.glassmorphism {
+  background: rgba(255, 255, 255, 0.2);
+  backdrop-filter: blur(10px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.3);
+}
+
+.dark .glassmorphism {
+  background: rgba(0, 0, 0, 0.2);
+  border: 1px solid rgba(255, 255, 255, 0.1);
+}
+
+@keyframes float {
+  0% {
+    transform: translateY(0px);
   }
-  
-  .dark .glassmorphism {
-    background: rgba(0, 0, 0, 0.2);
-    border: 1px solid rgba(255, 255, 255, 0.1);
+
+  50% {
+    transform: translateY(-20px);
   }
-  
-  @keyframes float {
-    0% { transform: translateY(0px); }
-    50% { transform: translateY(-20px); }
-    100% { transform: translateY(0px); }
+
+  100% {
+    transform: translateY(0px);
   }
-  
-  .float-animation {
-    animation: float 6s ease-in-out infinite;
-  }
-  </style>
+}
+
+.float-animation {
+  animation: float 6s ease-in-out infinite;
+}
+</style>
